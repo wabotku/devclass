aacddeca3667c9aa2d3e5f2adf9b9091ec457ba4
Walid Asyhari <walidshr@gmail.com>
2021-06-07 08:39:49 +0700

    first commit

---
+++ src/pages/landingPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="container-fluid">
+                <h2>Landing Page</h2>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
